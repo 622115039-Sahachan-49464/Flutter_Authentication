ead60bc6373c4f613ad26cdbbc452e721ba003b2
oat431 <oat431@gmail.com>
2022-01-27 13:42:11 +0700

    [🧪 add] : work today

---
+++ README.md
@@ -0,0 +1,5 @@
+# se492_flutter_auth
+
+Firebase Authentication
+
+work on : 27-01-2022
---
+++ android/app/src/main/kotlin/com/example/se492_flutter_auth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.se492_flutter_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
